03fb17889afaacc9d89f633ad4bbf46e592cf008
giancarlo sinuiri <gsinuiri@gmail.com>
2026-09-21 09:44:17 -0500

    chore: remove scratch files

--- run-ci.ps1
+++
@@ -1,8 +0,0 @@
-pnpm --filter @ayr/shared build
-pnpm --filter @ayr/api db:generate
-pnpm format:check
-pnpm lint
-pnpm typecheck
-pnpm test:scripts
-pnpm --filter @ayr/api test:cov
-pnpm --filter @ayr/web test:cov
